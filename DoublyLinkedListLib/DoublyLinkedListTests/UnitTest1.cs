2b09b1f99fd2aa854412a66f7658fdf01760a3db
RylanStevens <stevens.rylan@icloud.com>
2024-10-24 00:05:15 -0600

    I think this is the goal

---
+++ DoublyLinkedListLib/DoublyLinkedListTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DoublyLinkedListTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
